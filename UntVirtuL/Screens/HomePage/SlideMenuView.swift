df942a0d9995d9d397324433b24f669e92b2a95e
Baru Miranda <barafaelmiran@gmail.com>
2024-06-04 20:38:49 -0500

    FEAAT[BM] Implementation SliderMenu

---
+++ UntVirtuL/Screens/HomePage/SlideMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  SlideMenuView.swift
+//  UntVirtuL
+//
+//  Created by Baru Rafael Miranda Salas on 4/06/24.
+//
+
+import SwiftUI
+
+struct SlideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SlideMenuView()
+}
